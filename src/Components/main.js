951ab05d36fa8cb8e604a3d4831c50db9264b63c
Anirudh Bhat <56544816+anirudhjl@users.noreply.github.com>
2021-03-10 14:57:09 +0530

    Modified class to function component

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,100 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "../Styles/main/main.css";
 
-class MainPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      isLoggedIn: false,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  pageMover = (event) => {
-    if (this.state.email !== "" && this.state.password !== "") {
-      this.props.history.push("/signin");
-      this.setState({
-        isLoggedIn: true,
-      });
+const MainPage = () => {
+  let history = useHistory();
+  const [state, setState] = useState({ email: "", password: "" });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const pageMover = (event) => {
+    event.preventDefault();
+    if (state.email !== "" && state.password !== "") {
+      console.log(isLoggedIn);
+      history.push("/signin");
     } else event.preventDefault();
   };
-  handleChange = (event) => {
+  const handleChange = (event) => {
     event.preventDefault();
-    this.setState({
-      email: event.target.email,
-      password: event.target.password,
-    });
+    setState((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+    setIsLoggedIn(true);
   };
-  render() {
-    return (
-      <div id="page">
-        <h1>Travellogy</h1>
-        <p>Why stop when you can not!</p>
-        <br />
-        <button
-          className="btn btn-dark btn-lg"
-          id="myBtn"
-          type="button"
-          onClick={() => {
-            document.getElementById("signinModal").style.display = "block";
-          }}
-        >
-          Sign In
-        </button>{" "}
-        <button
-          className="btn btn-primary btn-lg"
-          type="button"
-          onClick={() => {
-            this.props.history.push("/signup");
-          }}
-        >
-          Sign Up
-        </button>
-        <div id="signinModal" className="modal">
-          <div className="modal-content">
-            <div
-              className="close"
-              onClick={() => {
-                document.getElementById("signinModal").style.display = "none";
-              }}
-            >
-              &times;
-            </div>
-            <form className="form-group" onSubmit={this.pageMover}>
-              <p>
-                <label>Email</label>
-              </p>
-              <input
-                type="email"
-                required
-                id="email"
-                onChange={this.handleChange}
-                className="form-control"
-              />
-              <br />
-              <p>
-                <label>Password</label>
-              </p>
-              <input
-                type="password"
-                id="password"
-                required
-                minLength="8"
-                className="form-control"
-                onChange={this.handleChange}
-              />
-              <br />
-              <br />
-              <button className="btn btn-dark" id="confirmbtn" type="submit">
-                Sign In
-              </button>
-            </form>
+
+  return (
+    <div id="page">
+      <h1>Travellogy</h1>
+      <p>Why stop when you can not!</p>
+      <br />
+      <button
+        className="btn btn-dark btn-lg"
+        id="myBtn"
+        type="button"
+        onClick={() => {
+          document.getElementById("signinModal").style.display = "block";
+        }}
+      >
+        Sign In
+      </button>{" "}
+      <button
+        className="btn btn-primary btn-lg"
+        type="button"
+        onClick={() => {
+          this.props.history.push("/signup");
+        }}
+      >
+        Sign Up
+      </button>
+      <div id="signinModal" className="modal">
+        <div className="modal-content">
+          <div
+            className="close"
+            onClick={() => {
+              document.getElementById("signinModal").style.display = "none";
+            }}
+          >
+            &times;
           </div>
+          <form className="form-group" onSubmit={pageMover}>
+            <p>
+              <label>Email</label>
+            </p>
+            <input
+              type="email"
+              required
+              name="email"
+              id="email"
+              value={state.name}
+              onChange={handleChange}
+              className="form-control"
+            />
+            <br />
+            <p>
+              <label>Password</label>
+            </p>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={state.name}
+              required
+              minLength="8"
+              className="form-control"
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+            <button className="btn btn-dark" id="confirmbtn" type="submit">
+              Sign In
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default MainPage;
